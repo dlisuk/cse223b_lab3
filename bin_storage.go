ded13ee41df3ce1394b3b0375c726519668e3d1d
dlisuk <dlisuk@gmail.com>
2014-04-24 00:16:20 -0700

    Improved hashing function to get better distribution

--- bin_storage.go
+++ bin_storage.go
@@ -3,19 +3,34 @@ package triblab
 import (
 	"trib"
 	"hash/fnv"
-	"encoding/base64"
+	//"encoding/base64"
+	"strconv"
 	"sort"
 )
 
 func hashBinKey(word string) string{
 	hasher := fnv.New32a()
-	return base64.URLEncoding.EncodeToString(hasher.Sum([]byte(word)))
+	hasher.Write([]byte("Jq0r6pLVtsXPNkVoliqAyvdZprpwtzPvgQk7WVmX"))
+	word2 := "SQA4ZC8m6DhmWhhPhKyN" + word
+	chars := make([]byte,3,3)
+	hasher.Write([]byte(word2))
+	for i := range(word2){
+		chars[0] = word2[i]
+		chars[2] = word2[len(word2)-i-1]
+		chars[1] = chars[1] + chars[0] + chars[2]
+		hasher.Write(chars)
+	}
+	return strconv.FormatInt(int64(hasher.Sum32()) % 1000000,10)
 }
 func NewBinClient(backs []string) trib.BinStorage {
 	backends := make([]backend,0,len(backs))
 	for i := range backs {
 		addr := backs[i]
+		print(addr)
+		print("-")
 		hash := hashBinKey(addr)
+		print(hash)
+		print("\n")
 
 		store := NewClient(addr)
 
@@ -30,11 +45,17 @@ type binClient struct{
 }
 
 func (self *binClient) Bin(name string) trib.Storage{
+	print(name)
 	hash := hashBinKey(name)
+	print(":")
+	print(hash)
+	print(":")
 	ind := sort.Search(len(self.backs), func(i int) bool{ return self.backs[i].hash >= hash})
 	if ind == -1 || ind == len(self.backs) {
 		ind = 0
 	}
+	print(self.backs[ind].addr)
+	print("\n")
 	return NewProxy(name,self.backs[ind].store)
 }
 
