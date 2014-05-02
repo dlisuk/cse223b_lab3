9ecf3e41d93e2b5837dd3a394ac3560b8be3b668
dlisuk <dlisuk@gmail.com>
2014-05-01 18:36:03 -0700

    Bin Client Should Goto Right Backend

--- bin_storage.go
+++ bin_storage.go
@@ -4,6 +4,8 @@ import (
 	"trib"
 	"hash/fnv"
 	"sort"
+	"strconv"
+	"errors"
 )
 
 func HashBinKey(word string) uint64{
@@ -40,7 +42,6 @@ type binClient struct{
 }
 
 func (self *binClient) Bin(name string) trib.Storage{
-	//TODO: Need to add code here to ensure we are pointing at the correct "master"
 	hash := HashBinKey(name)
 	ind := sort.Search(len(self.backs), func(i int) bool{ return self.backs[i].hash >= hash})
 	if ind == -1 || ind == len(self.backs) {
@@ -50,9 +51,9 @@ func (self *binClient) Bin(name string) trib.Storage{
 }
 
 type backend struct{
-	addr  string
-	hash  uint64
-	store trib.Storage
+	addr      string
+	hash      uint64
+	store     trib.Storage
 }
 
 type byHash []backend
@@ -60,3 +61,134 @@ func (v byHash) Len() int { return len(v) }
 func (v byHash) Swap(i, j int) { v[i], v[j] = v[j], v[i] }
 func (v byHash) Less(i, j int) bool { return v[i].hash < v[j].hash }
 
+func NewBinStorageProxy(bin string, backs []backend) trib.Storage {
+	backends := make([]backend,0,len(backs))
+	for _,b := range backs{
+		backends = append(backends, backend{
+				b.addr,
+				b.hash,
+				NewProxy(bin, b.store)})
+	}
+
+	return &binStorageProxy{
+		bin,
+		HashBinKey(bin),
+		backends,
+		nil }
+}
+
+func (self *binStorageProxy) checkBackend(back *backend) bool{
+	if back == nil{
+		return false
+	}
+	store := back.store
+	var result string
+	err := store.Get(MasterKeyLB, &result)
+	if err != nil{ return false }
+	masterlb, err := strconv.ParseUint(result,10,64)
+	if err != nil{ return false }
+
+	if masterlb < self.bin_hash && self.bin_hash <= back.hash || back.hash < masterlb && (self.bin_hash <= back.hash || masterlb < self.bin_hash ){
+		return true
+	}else{
+		return false
+	}
+}
+
+func (self *binStorageProxy) findBackend() *backend{
+	if self.checkBackend(self.bin_back){
+		return self.bin_back
+	}else{
+		self.bin_back = nil
+	}
+	for self.bin_back == nil{
+		for _, back := range self.all_backs{
+			if self.checkBackend(&back){
+				self.bin_back = &back
+			}
+		}
+	}
+	return self.bin_back
+}
+
+type binStorageProxy struct{
+	bin       string
+	bin_hash  uint64
+	all_backs []backend
+	bin_back  *backend
+}
+
+func (self *binStorageProxy) Get(key string, value *string) error{
+	err  := errors.New("FILLER")
+	for err != nil{
+		back := self.findBackend()
+		err   = back.store.Get(key,value)
+	}
+	return nil
+}
+
+func (self *binStorageProxy) Set(kv *trib.KeyValue, succ *bool) error{
+	err  := errors.New("FILLER")
+	for err != nil{
+		back := self.findBackend()
+		err   = back.store.Set(kv,succ)
+	}
+	return nil
+}
+
+func (self *binStorageProxy) Keys(p *trib.Pattern, list *trib.List) error{
+	list.L = make([]string,0)
+	err  := errors.New("FILLER")
+	for err != nil{
+		back := self.findBackend()
+		err   = back.store.Keys(p,list)
+	}
+	return nil
+}
+
+func (self *binStorageProxy) ListGet(key string, list *trib.List) error{
+	list.L = make([]string,0)
+	err  := errors.New("FILLER")
+	for err != nil{
+		back := self.findBackend()
+		err   = back.store.ListGet(key,list)
+	}
+	return nil
+}
+
+func (self *binStorageProxy) ListAppend(kv *trib.KeyValue, succ *bool) error{
+	err  := errors.New("FILLER")
+	for err != nil{
+		back := self.findBackend()
+		err   = back.store.ListAppend(kv,succ)
+	}
+	return nil
+}
+
+func (self *binStorageProxy) ListRemove(kv *trib.KeyValue, n *int) error{
+	err  := errors.New("FILLER")
+	for err != nil{
+		back := self.findBackend()
+		err   = back.store.ListRemove(kv,n)
+	}
+	return nil
+}
+
+func (self *binStorageProxy) ListKeys(p *trib.Pattern, list *trib.List) error{
+	list.L = make([]string,0)
+	err  := errors.New("FILLER")
+	for err != nil{
+		back := self.findBackend()
+		err   = back.store.ListKeys(p,list)
+	}
+	return nil
+}
+
+func (self *binStorageProxy) Clock(atLeast uint64, ret *uint64) error{
+	err  := errors.New("FILLER")
+	for err != nil{
+		back := self.findBackend()
+		err   = back.store.Clock(atLeast,ret)
+	}
+	return nil
+}
--- constants.go
+++ constants.go
@@ -2,6 +2,7 @@ package triblab
 
 const(
 	LogKey        = "issue_log"
+	ResLogKey     = "result_log"
 	CommittedKey  = "committed_clock"
 	MasterKeyLB   = "master_lb"
 	ReplicKeyLB   = "replicator_lb"
